d081c3de1965a015509585d98ca79031303b2c89
Mike <mclancy96@gmail.com>
2025-06-05 13:27:10 -0400

    Add home button

--- src/scripts/reset_quiz.js
+++ src/scripts/reset_quiz.js
@@ -8,16 +8,25 @@ const resetQuiz = () => {
 
 const createAndAppendResetButton = (buttonDiv) => {
   const button = document.createElement('button')
-  button.className = 'btn btn-lg btn-danger'
+  button.className = 'btn btn-lg btn-danger m-1'
   button.textContent = 'Restart Quiz'
   button.addEventListener('click', resetQuiz)
+  buttonDiv.appendChild(button)
+}
+
+const createAndAppendHomeButton = (buttonDiv) => {
+  const button = document.createElement('button')
+  button.className = 'btn btn-lg btn-info m-1'
+  button.textContent = 'Go Home'
+  button.addEventListener('click', () => location.reload())
   buttonDiv.appendChild(button)
 }
 
 const addResetButton = () => {
   const container = document.querySelector('#container');
   const buttonDiv = document.getElementById('resetButton')
-  buttonDiv.className = 'text-center position-absolute bottom-0 start-50 translate-middle-x py-2'
+  buttonDiv.className = 'text-center position-absolute bottom-0 start-50 translate-middle-x py-2 btn-group'
+  createAndAppendHomeButton(buttonDiv)
   createAndAppendResetButton(buttonDiv)
   container.appendChild(buttonDiv)
 }
